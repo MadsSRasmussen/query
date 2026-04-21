a2ade4a9028ecb02fc51641a7cc354cccf001ff4
Mads Studsgaard Rasmussen <madsrasmussendev@gmail.com>
2026-04-21 19:30:09 +0200

    docs: update mysql2 adapter docs

--- packages/query-mysql2/mod.ts
+++ packages/query-mysql2/mod.ts
@@ -1,3 +1,45 @@
+/**
+ * @example
+ * ```ts
+ * import { Store } from "@msrass/query";
+ *
+ * import { MySqlCompiler } from "@msrass/query/mysql";
+ * import { MySql2Executor } from "@msrass/query-mysql2";
+ *
+ * import mysql2 from "mysql2/promise";
+ *
+ * const pool = mysql2.createPool({
+ *     host: 'localhost',
+ *     user: 'root',
+ *     password: 'root',
+ *     database: 'test',
+ * });
+ *
+ * const compiler = new MySqlCompiler();
+ * const executor = new MySql2Executor(pool);
+ *
+ * type Database = {
+ *     users: { id: number; name: string };
+ *     posts: { id: number; content: string; user_id: number };
+ * };
+ *
+ * const store = new Store<Database>().withExecutor(compiler, executor);
+ *
+ * // The query can be now be crafted.
+ * const query = store.query("posts")
+ *     .join('users', 'users.id', 'posts.user_id')
+ *     .pick(['users.id', 'user_id'], 'users.name', 'posts.content', 'posts.id')
+ *     .where('users.id', 1);
+ *
+ * // And executed.
+ * const posts = await query.execute();
+ *
+ * console.log(posts);
+ * ```
+ *
+ * @module
+ */
+
 import type { CompiledMySql } from "@msrass/query/mysql";
 import type { CompiledQuery, Executor, QueryExecutor } from "@msrass/query";
 import type { Buffer } from "node:buffer";
@@ -5,17 +47,35 @@ import type { Buffer } from "node:buffer";
 type SqlPrimitive = string | number | boolean | null | Buffer | Date;
 type SqlValues = SqlPrimitive | SqlPrimitive[] | Record<string, SqlPrimitive>;
 
-export interface MySql2Pool {
+interface MySql2Pool {
     query(sql: string, values?: SqlValues[]): Promise<[unknown, unknown]>;
     execute(sql: string, values?: SqlValues[]): Promise<[unknown, unknown]>;
     getConnection(): Promise<MySql2PoolConnection>;
 }
 
-export interface MySql2PoolConnection {
+interface MySql2PoolConnection {
     release(): void;
     query(sql: string, values?: SqlValues[]): Promise<[unknown, unknown]>;
 }
 
+/**
+ * MySQL execution adapter for the npm:mysql2/promise package.
+ *
+ * @example
+ * ```ts
+ * import mysql2 from 'npm:mysql2/promise';
+ * import { MySql2Executor } from '@msrass/query-mysql2';
+ *
+ * const pool = mysql2.createPool({
+ *     host: 'localhost',
+ *     user: 'root',
+ *     password: 'root',
+ *     database: 'test',
+ * });
+ *
+ * const executor = new MySql2Executor(pool);
+ * ```
+ */
 export class MySql2Executor implements Executor<CompiledMySql> {
     private pool: MySql2Pool;
 
