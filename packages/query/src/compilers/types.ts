ac66b1467cb71bcb17850d41a86f95797eaaccfb
Mads Studsgaard Rasmussen <madsrasmussendev@gmail.com>
2026-05-02 18:35:26 +0200

    feat: implement compileDelete for mysql

--- packages/query/src/compilers/types.ts
+++ packages/query/src/compilers/types.ts
@@ -1,9 +1,11 @@
 import type { QueryCompiler } from "../query.ts";
 import type { WriteCompiler } from "../write.ts";
 import type { UpdateCompiler } from "../update.ts";
+import type { DeleteCompiler } from "../delete.ts";
 
 export interface Compiler<T extends unknown> {
     compileQuery: QueryCompiler<T>;
     compileWrite: WriteCompiler<T>;
     compileUpdate: UpdateCompiler<T>;
+    compileDelete: DeleteCompiler<T>;
 }
